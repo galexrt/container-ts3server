d0bde16ccf439daa8b8cfcce73e7935470fb6916
Alexander Trost <Galexrt@googlemail.com>
2015-10-03 22:08:58 +0200

    Changed the Vars to TS_*

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-TS_VERSION=`wget -O - https://www.server-residenz.com/tools/ts3versions.json | jsawk -n 'out(this.latest)'`
-TS3FILE="teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz"
+if [ -z "$TS_VERSION" ]; then
+    TS_VERSION=`wget -O - https://www.server-residenz.com/tools/ts3versions.json | jsawk -n 'out(this.latest)'`
+fi
+
+TSFILE="teamspeak3-server_linux-amd64-$TS_VERSION.tar.gz"
 
 if [ ! -f "/data/.ts3-installed" ]; then
-    wget -q "http://dl.4players.de/ts/releases/$TS_VERSION/$TS3FILE" -C /data
-    tar xf "/data/$TS3FILE" --strip-components=1 -C /data
+    wget -q "http://dl.4players.de/ts/releases/$TS_VERSION/$TSFILE" -C /data
+    tar xf "/data/$TSFILE" --strip-components=1 -C /data
     touch "/data/.ts3-installed"
 fi
 
-TS3ARGS="$@"
+TSARGS="$@"
 if [ -e /data/ts3server.ini ]; then
-  TS3ARGS="$TS3ARGS inifile=/data/ts3server.ini"
+  TSARGS="$TSARGS inifile=/data/ts3server.ini"
 else
-  TS3ARGS="$TS3ARGS createinifile=1"
+  TSARGS="$TSARGS createinifile=1"
 fi
 
-cd /data
-exec /data/ts3server_linux_amd64 "$TS3ARGS"
+exec /data/ts3server_linux_amd64 "$TSARGS"
