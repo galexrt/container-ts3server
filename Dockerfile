708cc2804448b3e56196dd5a81049d63324248e0
Alexander Trost <galexrt@googlemail.com>
2020-04-12 23:53:19 +0200

    docker: always use DATA_DIR env var in build and entrypoint
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- Dockerfile
+++ Dockerfile
@@ -6,27 +6,30 @@ ARG TS3_USER="3000"
 ARG TS3_GROUP="3000"
 ARG ARCH="linux_amd64"
 
-ENV TS3_DIR="/data" TS3SERVER_LICENSE="accept" TSDNS_ENABLE="False" LD_LIBRARY_PATH="/data" \
+ENV DATA_DIR="/data" TS3SERVER_LICENSE="accept" TSDNS_ENABLE="False" LD_LIBRARY_PATH="/data" \
     TS_VERSION="$TS_VERSION" TS3_USER="$TS3_USER" TS3_GROUP="$TS3_GROUP" ARCH="$ARCH"
 
 RUN groupadd -g "$TS3_GROUP" teamspeak && \
-    useradd -u "$TS3_USER" -g "$TS3_GROUP" -d "$TS3_DIR" teamspeak && \
+    useradd -u "$TS3_USER" -g "$TS3_GROUP" -d "$DATA_DIR" teamspeak && \
     apt-get -qq update && \
     apt-get -q upgrade -y && \
     DEBIAN_FRONTEND=noninteractive apt-get -q install -y wget ca-certificates bzip2 sudo && \
-    mkdir -p "$TS3_DIR" && \
-    wget -nv "https://files.teamspeak-services.com/releases/server/$TS_VERSION/teamspeak3-server_$ARCH-$TS_VERSION.tar.bz2" -O "/data/teamspeak-server.tar.bz2" && \
-    echo "$TS_VERSION" > /data/.downloaded && \
-    cd "$TS3_DIR" && \
-    bzip2 -d "$TS3_DIR/teamspeak-server.tar.bz2" && \
-    chown teamspeak:teamspeak -R "$TS3_DIR" && \
+    mkdir -p "$DATA_DIR" && \
+    wget -nv "https://files.teamspeak-services.com/releases/server/$TS_VERSION/teamspeak3-server_$ARCH-$TS_VERSION.tar.bz2" -O "$DATA_DIR/teamspeak-server.tar.bz2" && \
+    cd "$DATA_DIR" && \
+    bzip2 -d "$DATA_DIR/teamspeak-server.tar.bz2" && \
+    tar xfv "$DATA_DIR/teamspeak-server.tar" -C "$DATA_DIR" --strip-components 1 && \
+    rm "$DATA_DIR/teamspeak-server.tar" && \
+    echo "$TS_VERSION" > "$DATA_DIR/.downloaded" && \
+    chown teamspeak:teamspeak -R "$DATA_DIR" && \
     apt-get -qq autoremove -y --purge && \
     apt-get -qq clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 USER teamspeak
 
-VOLUME ["$TS3_DIR"]
+WORKDIR "$DATA_DIR"
+VOLUME ["$DATA_DIR"]
 EXPOSE 9987/udp 10011/tcp 30033/tcp 41144/tcp
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ if [ "$DEBUG" == "True" ] || [ "$DEBUG" == "true" ]; then
     set -xe
 fi
 
+DATA_DIR="${DATA_DIR:-/data}"
 DOWNLOAD_TS="${DOWNLOAD_TS:-True}"
 TS_VERSION="${TS_VERSION:-}"
 ARCH="${ARCH:-linux_amd64}"
@@ -12,45 +13,45 @@ ONLY_TSDNS="${ONLY_TSDNS:-False}"
 TSDNS_PORT="${TSDNS_PORT:-41144}"
 
 startTSDNS() {
-    cd /data/tsdns
+    cd "$DATA_DIR/tsdns"
     echo "=> Starting TSDNS server .."
-    exec /data/tsdns/tsdnsserver "$TSDNS_PORT"
+    exec "$DATA_DIR/tsdns/tsdnsserver" "$TSDNS_PORT"
 }
 
-cd /data || { echo "Can't access the data directory"; exit 1; }
+cd "$DATA_DIR" || { echo "Can't access the data directory ($DATA_DIR)"; exit 1; }
 
 if [ "$DOWNLOAD_TS" = "True" ] || [ "$DOWNLOAD_TS" = "true" ]; then
-    rm -rf "/data/teamspeak-server.tar"
+    rm -rf "$DATA_DIR/teamspeak-server.tar"
 
-    if [ ! -f "/data/.downloaded" ] || [ "$(cat /data/.downloaded)" != "$TS_VERSION" ]; then
+    if [ ! -f "$DATA_DIR/.downloaded" ] || [ "$(cat "$DATA_DIR/.downloaded")" != "$TS_VERSION" ]; then
         echo "->TS3 Server downloading Version $TS_VERSION ..."
-        wget -nv "https://files.teamspeak-services.com/releases/server/$TS_VERSION/teamspeak3-server_$ARCH-$TS_VERSION.tar.bz2" -O "/data/teamspeak-server.tar.bz2"
-        bzip2 -d "/data/teamspeak-server.tar.bz2"
-        tar xf "/data/teamspeak-server.tar" --strip-components=1 -C /data
-        rm -rf "/data/teamspeak-server.tar"
-        echo "$TS_VERSION" > "/data/.downloaded"
+        wget -nv "https://files.teamspeak-services.com/releases/server/$TS_VERSION/teamspeak3-server_$ARCH-$TS_VERSION.tar.bz2" -O "$DATA_DIR/teamspeak-server.tar.bz2"
+        bzip2 -d "$DATA_DIR/teamspeak-server.tar.bz2"
+        tar xf "$DATA_DIR/teamspeak-server.tar" -C "$DATA_DIR" --strip-components=1
+        rm "$DATA_DIR/teamspeak-server.tar"
+        echo "$TS_VERSION" > "$DATA_DIR/.downloaded"
         echo "=> TS Server Version $TS_VERSION download complete"
     fi
 fi
 
-[ ! -x "/data/ts3server_minimal_runscript.sh" ] && { echo "Couldn't find ts3server_minimal_runscript.sh. Exiting.."; exit 1; }
+[ ! -x "$DATA_DIR/ts3server_minimal_runscript.sh" ] && { echo "Couldn't find ts3server_minimal_runscript.sh. Exiting.."; exit 1; }
 
 if [ ! -z "$TS3_WHITELIST" ]; then
-    echo "" > /data/query_ip_whitelist.txt
+    echo "" > "$DATA_DIR/query_ip_whitelist.txt"
     for entry in $(echo "$TS3_WHITELIST" | tr ',' ' '); do
-        echo "$entry" >> /data/query_ip_whitelist.txt
+        echo "$entry" >> "$DATA_DIR/query_ip_whitelist.txt"
     done
 fi
 if [ ! -z "$TS3_BLACKLIST" ]; then
-    echo "" > /data/query_ip_blacklist.txt
+    echo "" > "$DATA_DIR/query_ip_blacklist.txt"
     for entry in $(echo "$TS3_BLACKLIST" | tr ',' ' '); do
-        echo "$entry" >> /data/query_ip_blacklist.txt.txt
+        echo "$entry" >> "$DATA_DIR/query_ip_blacklist.txt.txt"
     done
 fi
 
 TSARGS="$*"
-if [ -e "/data/ts3server.ini" ]; then
-    TSARGS="$TSARGS inifile=/data/ts3server.ini"
+if [ -e "$DATA_DIR/ts3server.ini" ]; then
+    TSARGS="$TSARGS inifile=$DATA_DIR/ts3server.ini"
 else
     TSARGS="$TSARGS createinifile=1"
 fi
